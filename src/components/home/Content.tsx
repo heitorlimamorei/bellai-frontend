ad02498cbf5914463344dc3e135e583130f2368d
Rafael Arrieiro <rafael.arrieiro@gmail.com>
2024-03-17 00:46:27 +0000

    content

--- src/components/home/Content.tsx
+++ src/components/home/Content.tsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import React from 'react';
 
 export default function Content() {
   return (
-    <div>Content</div>
-  )
+    <div className="hidden lg:flex flex-column mt-8 p-4 w-[60%] h-fit-content bg-blue-900">
+      <h1 className="text-white font-serif text-2xl p-5 text-left mr-8">
+        Sobre a origem do bullying
+      </h1>
+      <p className="text-white">
+        O bullying está presente em diversos âmbitos da sociedade, sobretudo
+        porque sua origem é determinada por uma falha na interação social. Sem
+        dúvida, o convívio ideal é fundamentado nos princípios da harmonia e da
+        paz, sabendo que as relações humanas apresentam uma finalidade comum: a
+        felicidade da totalidade do corpo social. Contudo, a violação dessa
+        concepção pode ser manifestada no bullying, o qual desagrega e rompe as
+        estruturas constituintes do bem comum. Logo, a segregação se revela como
+        nociva para a manutenção da ordem na conjuntura atual.
+      </p>
+    </div>
+  );
 }
